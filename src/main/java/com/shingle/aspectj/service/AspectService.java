0b861d41696052e1beee4e8eaf4c0b71b1c19b12
shingle2302 <shingle2302@qq.com>
2025-05-18 23:30:19 +0800

    feat:spring-boot老版本集成brave

--- src/main/java/com/shingle/aspectj/service/AspectService.java
+++ src/main/java/com/shingle/aspectj/service/AspectService.java
@@ -1,16 +1,38 @@
 package com.shingle.aspectj.service;
 
+import brave.Span;
+import brave.Tracer;
+import brave.Tracing;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AspectService {
 
+
+
     public String publicMethod() {
-        privateMethod();
-        return "publicMethod";
+        Tracer tracer = Tracing.currentTracer();
+        Span span = tracer.nextSpan().name("publicMethod");
+        span.start();
+        try {
+            privateMethod();
+            return "publicMethod";
+        } finally {
+            span.finish();
+
+        }
     }
 
     private String privateMethod() {
-        return "privateMethod";
+        Tracer tracer = Tracing.currentTracer();
+
+        Span span = tracer.nextSpan().name("publicMethod");
+        span.start();
+        try {
+            return "privateMethod";
+        } finally {
+            span.finish();
+
+        }
     }
 }
